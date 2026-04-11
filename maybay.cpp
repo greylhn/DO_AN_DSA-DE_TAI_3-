a58eb39e4de1c2b92ef7dcc3133627a1364e131d
trinhhoat02052006 <trinhvanhoat02052006@gmail.com>
2026-04-11 15:28:13 +0700

    Update maybay.cpp

--- maybay.cpp
+++ maybay.cpp
@@ -1,12 +1,27 @@
-#include<bits/stdc++.h>
-#include"maybay.h"
+#include<iostream>
+#include<cstring>
+#include <conio.h>   // _getch()
+#include <windows.h> // mau sac
 using namespace std;
+#define MAXMB 300
+#define MAXSHMB 16
+#define MAXLMB 41
+struct MAYBAY {
+    char SoHieuMB[MAXSHMB];
+    char LoaiMB[MAXLMB];
+    int SoCho;
+};
+
+struct DSMAYBAY {
+    int n = 0;
+    MAYBAY* mb[MAXMB];
+};
 //tim may bay 
 int timmaybay (DSMAYBAY &ds , char soHieu[])
 {
 	for (int i = 0 ; i <ds.n ; i++)
 	{
-		if (strcmp(ds.mb[i]->SoHieuMB , SoHieu)== 0) return i ;
+		if (strcmp(ds.mb[i]->SoHieuMB , soHieu)== 0) return i ;
 	}
 	return -1 ;
 }
@@ -55,7 +70,7 @@ void xoamaybay (DSMAYBAY &ds)
 		return ;
 	}
 	delete ds.mb[pos];
-	for (int i = pos ; i < ds.n ; i++)
+	for (int i = pos ; i < ds.n-1 ; i++)
 	{
 		ds.mb[i] = ds.mb[i+1] ;
 	}
@@ -72,10 +87,7 @@ void suamaybay(DSMAYBAY &ds){
         cout<<"Khong tim thay!\n";
         return;
     }
-
     cout<<"Nhap lai thong tin may bay:\n";
-
-    cin.ignore();
     cout<<"Nhap loai moi: ";
     cin.getline(ds.mb[pos]->LoaiMB,MAXLMB);
 
@@ -122,7 +134,9 @@ void OpenFile(DSMAYBAY &ds, char *filename)
 {
     FILE *f;
     MAYBAY mb;
-    ds.n = 0;
+    for(int i = 0; i < ds.n; i++)
+    delete ds.mb[i];
+     ds.n = 0;
 
     if ((f = fopen(filename, "rb")) == NULL)
     {
@@ -146,3 +160,132 @@ void DeleteItem (DSMAYBAY &ds, int i){
        ds.mb[j-1]=ds.mb[j];
     ds.n--;
 	 }
+	 //=================menu================
+///set mau
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+// ===== menu =====
+int menu() {
+    string items[] = {
+        "1.Cap nhat danh sach may bay",
+        "2.Cap nhat chuyen bay",
+        "3.Dat ve",
+        "4.Huy ve" ,
+        "5.In danh sach cac hanh khach trong 1 chuyen bay dua vao ma chuyen bay.",
+        "6.In danh sach cac chuyen bay khoi hanh trong  ngay dd/mm/yyyy den noi XXXX ma con ve.",
+        "7.In danh sach cac ve con trong cá»§a 1 chuyen bay co ma chuyen bay la X." ,
+        "8.Thong ke so luot thuc hien chuyen bay cua tung may bay theo thu tu so luot thuc hien giam dan." ,
+        "9.Thoat chuong trinh"
+    };
+
+    int n = 9;
+    int pos = 0;
+
+    while (true) {
+        system("cls");
+
+        cout << "=========== MENU ===========" << endl;
+
+        for (int i = 0; i < n; i++) {
+            if (i == pos) {
+                setColor(10); // mÃ u xanh
+                cout << " >> " << items[i] << endl;
+                setColor(7);  // reset
+            } else {
+                cout << "    " << items[i] << endl;
+            }
+        }
+
+        int ch = _getch();
+
+        if (ch == 224) { // phÃ­m Ä‘áº·c biá»‡t
+            ch = _getch();
+            if (ch == 72) pos--; // â†‘
+            else if (ch == 80) pos++; // â†“
+
+            if (pos < 0) pos = n - 1;
+            if (pos >= n) pos = 0;
+        }
+        else if (ch == 13) { // Enter
+            return pos;
+        }
+    }
+}
+//menu con quan li maybay
+int menuMayBay() {
+    string items[] = {
+        "1. Them may bay",
+        "2. Xoa may bay",
+        "3. Sua thong tin may bay",
+        "4. Liet ke danh sach may bay",
+        "5. Quay lai",
+        "6. luu file",
+        "7. Doc file"
+    };
+
+    int n = 7;
+    int pos = 0;
+
+    while (true) {
+        system("cls");
+        cout << "===== QUAN LY MAY BAY =====\n";
+
+        for (int i = 0; i < n; i++) {
+            if (i == pos) {
+                setColor(10);
+                cout << " >> " << items[i] << endl;
+                setColor(7);
+            } else {
+                cout << "    " << items[i] << endl;
+            }
+        }
+
+        int ch = _getch();
+
+        if (ch == 224) {
+            ch = _getch();
+            if (ch == 72) pos--;
+            else if (ch == 80) pos++;
+
+            if (pos < 0) pos = n - 1;
+            if (pos >= n) pos = 0;
+        }
+        else if (ch == 13) {
+            return pos;
+        }
+    }
+}
+int main ()
+{
+	DSMAYBAY ds ;
+	ds.n = 0 ;
+	int a[100], n = 0;
+	
+	while (true) {
+	    int choice = menu();
+	    system("cls");
+	
+	    if (choice == 0) {   // Cáº­p nháº­t mÃ¡y bay
+	
+	        while (true) {
+	            int c = menuMayBay();
+	
+        if (c == 0) themmaybay(ds);
+        else if (c == 1) xoamaybay(ds);
+        else if (c == 2) suamaybay(ds);
+        else if (c == 3) xuatDSMB(ds);
+        else if (c == 5) SaveFile(ds, (char*)"maybay.dat");
+        else if (c == 6) OpenFile(ds, (char*)"maybay.dat");
+        else break;
+	            cout << endl << "Nhan phim bat ky..." << endl;
+	            _getch();
+	        }
+	
+}
+}
+}
+
+	 
+	 
