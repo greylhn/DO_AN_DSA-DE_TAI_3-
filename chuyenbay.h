24d94697f01ebf59a12fa6f2e6ef55125a12955c
greylhn <lehuunghia1710@gmail.com>
2026-04-12 22:37:34 +0700

    Refactor chuyenbay.h structure and function definitions

--- chuyenbay.h
+++ chuyenbay.h
@@ -1,25 +1,37 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
 #include "Datetime.h"
 #include"ve.h"
 #define MAXMCB 16
 #define MAXSBD 50
 #define MAXSHMB 16
+using namespace std ;
 struct CHUYENBAY {
     char MaCB[MAXMCB];
     DateTime NgayGioKH;
     char SanBayDen[MAXSBD];
-    int TrangThai; 
+    int TrangThai; // 0  
     char SoHieuMB[MAXSHMB];
     DSVE dsve;
 };
-
-struct NodeCB {
+// chi quan tam den chuyen bay nay va ke tiep
+struct NoCB {
     CHUYENBAY data;
-    NodeCB* next;
+    NoCB* next;
 };
-typedef NodeCB* DSCB;
-void themCB (DSCB &a) ;
-void inCB (CHUYENBAY cb);
-void indsCB (DSCB ds) ;
-void xoaCB (DSCB &a) ;
-
+typedef NoCB* NodeCB;
+struct listCB 
+{
+	NodeCB head ;
+	NodeCB tail ;
+	int soLuongCB ;
+};
+NodeCB makeNodeCB (CHUYENBAY a);
+void insert_order (listCB &ls , CHUYENBAY a);
+void load_file_dscb (listCB &ls) ;
+void save_file_dscb(listCB &ls) ;
+NodeCB check_maCB (listCB ls, string ma) ;
+void themCB (listCB &ls) ;
+void chinh_Datetime (listCB &ls) ;
+void huyCB (listCB &ls) ;
+void in_danh_sach_cb(listCB ls) ;
